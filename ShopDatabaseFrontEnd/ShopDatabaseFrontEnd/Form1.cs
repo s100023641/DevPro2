fdd4d29e3cbd101e03d805ce1e588a85611a3cd1
Luke Sutherland <lukesutherland@gmail.com>
2016-04-12 12:41:32 +1000

    added basic front end GUI

---
+++ ShopDatabaseFrontEnd/ShopDatabaseFrontEnd/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace ShopDatabaseFrontEnd
+{
+    public partial class Form1 : Form
+    {
+        public Sale CurrentSale;
+        public Form1()
+        {
+            CurrentSale = new Sale();
+            InitializeComponent();
+            SetListBox();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void SetListBox() {
+            listBox1.Items.Clear();
+            string connStr = "server=localhost;user=root;database=pharmacydb;port=3306;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                Random r= new Random();
+                int orderNumb=r.Next();
+                // Perform database operations
+                string sqlText = "SELECT * FROM `stockableitems`";
+                Console.WriteLine(sqlText);
+                MySqlCommand cmd = new MySqlCommand(sqlText, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                
+                while(rdr.Read()){
+                listBox1.Items.Add(rdr[1]);
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            conn.Close();
+            Console.WriteLine("Done.");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Sale.SaleItem SI = new Sale.SaleItem(listBox1.Text, listBox1.SelectedIndex+1, int.Parse(textBox2.Text), double.Parse(textBox1.Text));
+            Console.WriteLine(SI.buildSQL("44"));
+            textBox3.Text += SI.buildSQL("44");
+
+            CurrentSale.items.Add(SI);
+            return;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CurrentSale.RecordToDatabase();
+            CurrentSale = new Sale();
+        }
+    }
+}
---
+++ ShopDatabaseFrontEnd/ShopDatabaseFrontEnd/Sale.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace ShopDatabaseFrontEnd
+{
+    public class Sale
+    {
+
+        public List<SaleItem> items;
+        public Sale() {
+            items = new List<SaleItem>();
+        }
+        public class SaleItem{
+
+            public SaleItem(string nameA, int numA, int qA, double pA) {
+                name = nameA;
+                itemNumber = numA;
+                quanity = qA;
+                price = pA;
+            }
+            public int itemNumber;
+            public string name;
+            public int quanity;
+            public double price;
+
+            public string buildSQL(string SaleID) {
+                
+                return string.Format("INSERT INTO `orderitem` (`OrderNum`, `ItemID`, `ItemAmount`, `Price`) VALUES ({0}, {1}, {2}, {3:N2});", SaleID, itemNumber, quanity, price);
+            }
+        }
+
+        public double TotalCost {
+            get {
+                double total = 0;
+                foreach (var item in items)
+                {
+                    total += item.price;
+                }
+                return total;
+            }
+        }
+
+        public void RecordToDatabase() {
+            string connStr = "server=localhost;user=root;database=pharmacydb;port=3306;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                Random r= new Random();
+                int orderNumb=r.Next();
+                // Perform database operations
+                string sqlText = string.Format("INSERT INTO `ordersale` (`OrderNum`, `Timestamp`) VALUES ({0}, CURRENT_TIMESTAMP)", orderNumb);
+                Console.WriteLine(sqlText);
+                MySqlCommand cmd = new MySqlCommand(sqlText, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                rdr.Close();
+                foreach (var item in items)
+                {
+                    string itemSQL = item.buildSQL(orderNumb.ToString());
+                    Console.WriteLine(itemSQL);
+                    MySqlCommand cmd2 = new MySqlCommand(itemSQL, conn);
+                    MySqlDataReader rdr2 = cmd2.ExecuteReader();
+                    rdr2.Close();
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            conn.Close();
+            
+            Console.WriteLine("Done.");
+               //insert sale at currentTime
+                
+
+            //close database boiler plate code;
+        }
+    }
+}
